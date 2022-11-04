f0cfbb8ad53beab520f78ad2d8b22c306ed537ed
Diana Chistyakova <103204655+Shirayuky@users.noreply.github.com>
2022-11-04 18:12:22 +0300

    The final

--- app.py
+++ app.py
@@ -4,15 +4,39 @@ from typing import List
 from flask import Flask, render_template
 import utils
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
-def title_page():
+def list_page():
     """Главная страница - вывод списком всех кандидатов"""
     candidates: List[dict] = utils.load_data()
     return render_template('list.html', candidates=candidates)
+
+
+@app.route("/candidate/<int:idx>")
+def card_page(idx):
+    """Вывод кандидата по id"""
+    candidate_id_card: dict = utils.get_candidate_id(idx)
+    if not candidate_id_card:
+        return "Кандидат не найден"
+    return render_template('card.html', candidate=candidate_id_card)
+
+
+@app.route("/search/<name>")
+def search_candidates_page(name):
+    """Вывод кандидата по id"""
+    candidate_name: List[dict] = utils.get_candidate_by_name(name)
+    return render_template('search.html', candidates=candidate_name)
+
+
+@app.route("/skill/<skill>")
+def search_skill_page(skill):
+    """Вывод кандидата по id"""
+    candidate_skill_card: List[dict] = utils.get_candidate_by_skills(skill)
+    if not candidate_skill_card:
+        return "У вас слишком завышенные ожидания!"
+    return render_template('skill.html', candidates=candidate_skill_card, skill=skill)
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -8,46 +8,48 @@
 #     "age": 40,
 #     "skills": "go, python"
 from __future__ import annotations
-import requests
-import json
 from typing import List
+import json
 
-# Включение загрузки
-def get_all_candidates() -> List[dict]:
-    """Включает загрузку данных"""
-    return load_data()
 
 # Загрузка
 def load_data() -> List[dict]:
     """Загружает список данных(как dict) из json файла"""
-    path = requests.get("https://jsonkeeper.com/b/A8VV", verify=False)
+    with open('candidates.json', 'r', encoding='utf-8') as file:
+        return list(json.load(file))
 
-    with open(path) as file:
-        candidates = list(json.load(file))
-        return candidates
+
+# print(f"База: {load_data()}")
 
 
 # Поиск/вывод данных по ключу
-def get_candidate_id (candidate_id: int) -> dict:
-    for candidate in get_all_candidates():
+def get_candidate_id(candidate_id: int) -> dict:
+    for candidate in load_data():
         if candidate['id'] == candidate_id:
             return candidate
+
+
+# print(f"Поиск по id: {get_candidate_id(4)}")
 
 
 # Поиск/вывод данных по значению=имя
 def get_candidate_by_name(candidate_name: str) -> List[dict]:
     result = []
-    for candidate in get_all_candidates():
-        if candidate['name'].title() == candidate_name:
+    for candidate in load_data():
+        if candidate['name'] == candidate_name:
             result.append(candidate)
     return result
+
+
+# print(f"Поиск по имени: {get_candidate_by_name('Day')}")
 
 
 # Поиск/вывод данных по значению=навык
 def get_candidate_by_skills(candidate_skill: str) -> List[dict]:
     result = []
-    for candidate in get_all_candidates():
-        if candidate_skill in candidate['skill'].lower().split(', '):
+    for candidate in load_data():
+        if candidate_skill.lower() in candidate['skills'].lower().split(', '):
             result.append(candidate)
     return result
 
+# print(f"Поиск по навыку: {get_candidate_by_skills('python')}")
